c6f8c2b805b8fba5243aeeb587b1fb754df78449
ujjwalravani <93965052+ujjwalravani@users.noreply.github.com>
2023-07-09 11:03:15 +0530

    Add files via upload

---
+++ backend/data/data.js
@@ -0,0 +1,110 @@
+const chats = [
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Madhav Kamani",
+          email: "madhavkamani17@gmail.com",
+        },
+        {
+          name: "Ujjwal Ravani",
+          email: "ujjwalravani21@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7c4dd4",
+      chatName: "Madhav Kamani",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Aryaman Bansal",
+          email: "aryamanbansal03@gmail.com",
+        },
+        {
+          name: "Shaleen Kakkad",
+          email: "shaleenkakkad@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468b27c4dd4",
+      chatName: "Aryaman Bansal",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Madhav Kamani",
+          email: "madhavkamani17@gmail.com",
+        },
+        {
+          name: "Aum Patel",
+          email: "aumpatel08@gmail.com",
+        },
+      ],
+      _id: "617a077e18c2d468bc7c4dd4",
+      chatName: "Madhav Kamani",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Madhav Kamani",
+          email: "madhavkamani17@gmail.com",
+        },
+        {
+          name: "Ujjwal Ravani",
+          email: "ujjwalravani21@gmail.com",
+        },
+        {
+          name: "Aryaman Bansal",
+          email: "aryamanbansal03@.com",
+        },
+      ],
+      _id: "617a518c4081150716472c78",
+      chatName: "Friends",
+      groupAdmin: {
+        name: "Madhav Kamani ",
+        email: "madhavkamani17@gmail.com",
+      },
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Aum Patel",
+          email: "aumpatel08@gmail.com",
+        },
+        {
+          name: "Aryaman Bansal",
+          email: "aryamanbansal03@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7cfdd4",
+      chatName: "Aum Patel",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Aum Patel",
+          email: "aumpatel08@gmail.com",
+        },
+        {
+          name: "Madhav Kamani",
+          email: "madhavkamani17@gmail.com",
+        },
+        {
+          name: "Shaleen Kakkad",
+          email: "shaleenkaddad@gmail.com",
+        },
+      ],
+      _id: "617a518c4081150016472c78",
+      chatName: "Our Group",
+      groupAdmin: {
+        name: "Aum Patel",
+        email: "aumpatel08@gmail.com",
+      },
+    },
+  ];
+
+module.exports = chats;
---
+++ backend/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+//const chats = require("./data/data");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const color  = require("colors");
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+
+
+const app = express();
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.send("API is runing succesfully");
+});
+
+//app.get('/api/chat', (req,res) => {
+//    res.send(chats); 
+//});
+
+app.get('/api/chat/:id', (req,res) => {
+    console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
+
+const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: "http://localhost:3000",
+      //credentials: true,
+    },
+  });
+
+io.on("connection", (socket) => {
+    console.log("connected to socket.io");
+    socket.on('setup', (userData) => {
+        socket.join(userData._id);
+        //console.log(userData._id);
+        socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User Joined Room: " + room);
+      });
+
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+    
+    socket.on("new message", (newMessageRecieved) => {
+        var chat = newMessageRecieved.chat;
+
+        if (!chat.users) return console.log("chat.users not defined");
+
+        chat.users.forEach((user) => {
+            if (user._id == newMessageRecieved.sender._id) return;
+
+            socket.in(user._id).emit("message recieved", newMessageRecieved);
+        });
+    });
+
+});
